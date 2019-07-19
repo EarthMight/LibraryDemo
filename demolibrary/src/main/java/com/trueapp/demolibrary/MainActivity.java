a90932e9b387813c25c42bdf2197f24e1c0c5391
EarthMight <humangod10@gmail.com>
2019-07-19 12:04:24 +0530

    Adding Layout Button into Activity main

--- demolibrary/src/main/java/com/trueapp/demolibrary/MainActivity.java
+++ demolibrary/src/main/java/com/trueapp/demolibrary/MainActivity.java
@@ -2,12 +2,16 @@ package com.trueapp.demolibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public  Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      button =findViewById(R.id.button);
+
     }
 }
